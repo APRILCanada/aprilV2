2b110e5dc9b3210140373b37b014087d68640c64
april.outofbox@gmail.com <april.outofbox@gmail.com>
2022-08-08 15:55:04 -0400

    created selectors: selectBroker and selectMarketId

--- src/app/flashquote/selectors/index.ts
+++ src/app/flashquote/selectors/index.ts
@@ -5,9 +5,19 @@ import { State } from '../store';
 export const selectForm = createFeatureSelector<State['form']>('form');
 
 // selectors
+export const selectBroker = createSelector(
+  selectForm,
+  (state) => state.broker
+);
+
 export const selectQuestions = createSelector(
   selectForm,
   (state) => state.questions
+);
+
+export const selectMarketId = createSelector(
+  selectForm,
+  (state) => state.broker.marketId
 );
 
 export const selectAllQuestionsLoaded = createSelector(
