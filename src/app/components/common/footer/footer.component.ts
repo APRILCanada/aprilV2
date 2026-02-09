d279f18b7b7841587d940ac35475b3f3add0b4d0
mariam-hm <hamm.mariam@gmail.com>
2026-02-09 12:00:51 -0500

    Added whistleblowing to the footer

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../../../services/language.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  constructor(public language: LanguageService) {}
 
   ngOnInit(): void {}
 }
